27505ebf3719ba74246b4eeae284245dd2a29b27
unknown <albarkhan60@gmail.com>
2024-02-05 13:08:02 +0530

    header ui completed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,23 @@ export default function App() {
 }
 
 function Header() {
-  return <div>HEader</div>;
+  return (
+    <header>
+      <nav className="navbar">
+        <div className="greetings">
+          <p>Goody Day Albar Khan</p>
+        </div>
+        <div className="logo">
+          <img src="https://bankist.netlify.app/logo.png" alt="logo"></img>
+        </div>
+        <div className="login">
+          <input className="user" placeholder="user"></input>
+          <input className="id" placeholder="id"></input>
+          <i class="fa-solid fa-arrow-right"></i>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 function Main() {
